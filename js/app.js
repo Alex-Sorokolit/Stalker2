542937cd59bd00bdf72ff626c602c3f8df3819a4
Alex-Sorokolit <sorokolital@gmail.com>
2023-02-06 16:47:09 +0200

    newImages

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-new Swiper(".swiper");
+new Swiper(".swiper", {
+  speed: 2400,
+  mousewheel: true,
+  loop: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  // spaceBetween: 50,
+  // grabCursor: true,
+  parallax: true,
+});
