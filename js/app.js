6526f5a9c52013dccc264715a9bbbb63c516764e
Alex-Sorokolit <sorokolital@gmail.com>
2023-02-06 23:34:47 +0200

    audio

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 new Swiper(".swiper", {
+  // direction: "vertical",
   speed: 2400,
-  mousewheel: true,
+  mousewheel: {
+    enabled: true,
+    sensitivity: 1,
+  },
   loop: true,
   keyboard: {
     enabled: true,
@@ -9,4 +13,21 @@ new Swiper(".swiper", {
   // spaceBetween: 50,
   // grabCursor: true,
   parallax: true,
+  // freeMode: true,
 });
+
+const audio = document.getElementById("audio");
+const playPauseBtn = document.getElementById("playPauseBtn");
+let count = 0;
+
+function playPause() {
+  if (count == 0) {
+    count = 1;
+    audio.play();
+    playPauseBtn.classList.toggle("play");
+  } else {
+    count = 0;
+    audio.pause();
+    playPauseBtn.classList.toggle("play");
+  }
+}
